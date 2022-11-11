da8c7fbec07fc134b6d76bf5a1485f4178a780f8
mainzonder <no7au53r@gmail.com>
2022-11-11 17:13:12 +0200

    c7

--- src/App.js
+++ src/App.js
@@ -1,64 +1,55 @@
 import React from "react";
 import axios from "axios";
-import {connect} from "react-redux";
+import {useDispatch} from "react-redux";
 
 import {Header} from "./components";
 import {Home, Cart} from "./pages";
 import {Route} from "react-router-dom";
-import {setPizzas as setPizzasAction} from "./redux/actions/pizzas";
+import {setPizzas} from "./redux/actions/pizzas";
 
-// function App() {
-// 	const [pizzas, setPizzas] = React.useState([]);
+function App() {
+	const dispatch = useDispatch();
 
-// 	React.useEffect(() => {
-// 		axios.get("http://localhost:3000/db.json").then(({data}) => {
-// 			setPizzas(data.pizzas);
-// 		});
-// 	}, []);
-// }
-
-class App extends React.Component {
-	componentDidMount() {
-		axios.get("http://localhost:3000/db.json").then(({data}) => {
-			this.props.setPizzas(data.pizzas);
+	React.useEffect(() => {
+		axios.get("http://localhost:3001/pizzas").then(({data}) => {
+			dispatch(setPizzas(data));
 		});
-	}
+	}, []);
 
-	render() {
-		return (
-			<>
-				<div className="wrapper">
-					<Header />
+	return (
+		<>
+			<div className="wrapper">
+				<Header />
 
-					<div className="content">
-						<Route
-							path="/"
-							render={() => <Home items={this.props.items} />}
-							exact
-						/>
-						<Route
-							path="/cart"
-							component={Cart}
-							exact
-						/>
-					</div>
+				<div className="content">
+					<Route
+						path="/"
+						component={Home}
+						exact
+					/>
+					<Route
+						path="/cart"
+						component={Cart}
+						exact
+					/>
 				</div>
-			</>
-		);
-	}
+			</div>
+		</>
+	);
 }
 
-const mapStateToProps = (state) => {
-	return {
-		items: state.pizzas.items,
-		filters: state.filters,
-	};
-};
+export default App;
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		setPizzas: (items) => dispatch(setPizzasAction(items)),
-	};
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+/* export default connect(
+	(state) => {
+		return {
+			items: state.pizzas.items,
+			filters: state.filters,
+		};
+	},
+	(dispatch) => {
+		return {
+			setPizzas: (items) => dispatch(setPizzas(items)),
+		};
+	}
+)(App); */
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,73 +1,34 @@
-import React, { useState } from 'react';
+import React from "react";
 
-// class Categories extends React.Component {
-// state={
-//   activeItem: 0
+const Categories = React.memo(function Categories({items, onClickItem}) {
+	const [activeItem, setActiveItem] = React.useState(null);
 
-// }
+	const onSelectItem = (index) => {
+		setActiveItem(index);
+		onClickItem(index);
+	};
 
-// onSelectItem = (index )=>{
-//   this.setState({
-//     activeItem: index,
-//   });
-// }
+	return (
+		<div className="categories">
+			<ul>
+				<li
+					onClick={() => onSelectItem(null)}
+					className={activeItem === null ? "active" : ""}>
+					Все
+				</li>
 
-//   render() {
-
-// const {items , onClickItem} = this.props;
-
-//     return (
-//       <div className='categories'>
-//         <ul>
-//           <li >Все</li>
-//           {items.map((name, index) => (
-//             <li
-//              className = {this.state.activeItem === index ? 'active' : ''}
-//               onClick={() => this.onSelectItem(index)}
-//               key={`${name}_${index}`}>
-//               {name}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-function Categories({ items, onClickItem }) {
-  // const [state, setState]  = useState(initialState)
-
-  const [activeItem, setActiveItem] = React.useState(null);
-
-
-
-  const onSelectItem = (index) => {
-    setActiveItem(index);
-
-  };
-
-
-
-  return (
-    <div className='categories'>
-      <ul>
-
-        <li onClick={() => onSelectItem(null)}
-          className={activeItem === null ? 'active' : ''
-          }>Все</li>
-
-        {items &&
-          items.map((name, index) => (
-            <li
-              className={activeItem === index ? 'active' : ''}
-              onClick={() => onSelectItem(index)}
-              key={`${name}_${index}`}>
-              {name}
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-}
+				{items &&
+					items.map((name, index) => (
+						<li
+							className={activeItem === index ? "active" : ""}
+							onClick={(even) => onSelectItem(index)}
+							key={`${name}_${index}`}>
+							{name}
+						</li>
+					))}
+			</ul>
+		</div>
+	);
+});
 
 export default Categories;
